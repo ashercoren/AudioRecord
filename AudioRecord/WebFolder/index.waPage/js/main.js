148cecbe756a5dd5f9a5045f56bfb113211597dd
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2013-10-02 12:48:20 -0400

    Update #5
    removed unneccsery details from screen

--- AudioRecord/WebFolder/index.waPage/js/main.js
+++ AudioRecord/WebFolder/index.waPage/js/main.js
@@ -1,29 +1,8 @@
-﻿$(function() {
-  var gain = $('#gain')[0];
-  var silenceLevel = $('#silenceLevel')[0];
-  for(var i=0; i<=100; i++) {
-    gain.options[gain.options.length] = new Option(100-i);
-    silenceLevel.options[silenceLevel.options.length] = new Option(i);
-  }
-
+﻿window.onload = function() {
   var appWidth = 24;
   var appHeight = 24;
   var flashvars = {'event_handler': 'microphone_recorder_events', 'upload_image': 'images/upload.png'};
   var params = {};
   var attributes = {'id': "recorderApp", 'name':  "recorderApp"};
   swfobject.embedSWF("recorder.swf", "flashcontent", appWidth, appHeight, "10.1.0", "", flashvars, params, attributes);
-});
-
-function configureMicrophone() {
-  if(! Recorder.isReady) {
-    return;
-  }
-
-  //Recorder.configure($('#rate').val(), $('#gain').val(), $('#silenceLevel').val(), $('#silenceTimeout').val());
-  //Recorder.setUseEchoSuppression($('#useEchoSuppression').is(":checked"));
-  //Recorder.setLoopBack($('#loopBack').is(":checked"));
-  Recorder.configure(22, 100, 0, 2000);
-  Recorder.setUseEchoSuppression(true);
-  Recorder.setLoopBack(false);
-  
 }
